fa913b7df48c5a59723ead8a5e605df40b0cc867
pranit3112 <shirolepranit3112@gmail.com>
2026-09-24 13:29:45 +0530

    test full pipeline

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from CI/CD pipeline!\n');
+  res.end('Hello from CI/CD pipeline! v2\n');
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
+
